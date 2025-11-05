635425ca6dbc7b2516d280167cb2ba45f5547903
Abdul Basit Ali <240479325+UITU-25SP-009-ST@users.noreply.github.com>
2025-11-05 10:23:59 +0000

    Lab-2 Completed

--- Lab-1/Main.java
+++ Lab-1/Main.java
@@ -1,8 +1,35 @@
+// OOPs Lab - 1
+
+// Task - 1
+// Abdul Basit Ali : 25SP-009-ST
+// public class Main{
+//     public static void main(String[] args){
+//         System.out.println("Hello, Abdul Basit ALi");
+//     }
+// }
 // Task - 2
 // Abdul Basit Ali : 25SP-009-ST
-public class Main{
-    public static void main(String[] args){
-        System.out.println("Hello, Abdul Basit ALi");
-        System.out.println("Institute: UIT University");
+// public class Main{
+//     public static void main(String[] args){
+//         System.out.println("Hello, Abdul Basit ALi");
+//         System.out.println("Institute: UIT University");
+//     }
+// }
+
+
+// Task - 3
+// Abdul Basit Ali : 25SP-009-ST
+public class Main {
+    public static void main(String[] args) {
+        String name = "Abdul Basit Ali";
+        System.out.println("Assalamu Alaikum, " + name + "! Hope you're having a productive day!");
     }
 }
+// Task - 5
+// Abdul Basit Ali : 25SP-009-ST
+// public class Main{
+//     public static void main(String[] args){
+//         System.out.println("Hello, Abdul Basit ALi")
+//         System.out.println("Institute: UIT University");
+//     }
+// }
