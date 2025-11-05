badbf34d37de87161b7b039f99c50a1bf8db103b
Abdul Basit Ali <240479325+UITU-25SP-009-ST@users.noreply.github.com>
2025-11-05 10:27:15 +0000

    Lab-2 Completed

---
+++ Lab-2/Main.java
@@ -0,0 +1,57 @@
+// OOPs : Lab-2
+
+// Task - 1
+// Abdul Basit Ali : 25SP-009-ST
+// public class Main{
+//     public static void main(String[] args){
+//         String name = "Abdul Basit Ali";
+//         int age = 20;
+//         System.out.println("Name: " + name + " - Age: " + age);
+//     }
+// }
+
+// Task - 2
+// Abdul Basit Ali : 25SP-009-ST
+// import java.util.Scanner;
+// public class Main{
+//     public static void main(String[] args){
+//         Scanner in = new Scanner(System.in);
+//         System.out.print("Enter a = ");
+//         int a = in.nextInt();
+//         System.out.print("Enter b = ");
+//         int b = in.nextInt();
+//         System.out.println("Sum is " + (a + b));
+//     }
+// }
+
+// Task - 3
+// Abdul Basit Ali : 25SP-009-ST
+// import java.util.Scanner;
+// public class Main{
+//     public static void main(String[] args){
+//         Scanner in = new Scanner(System.in);
+//         System.out.print("Enter first name : ");
+//         String fName = in.nextLine();
+//         System.out.print("Enter grades : ");
+//         float grade = in.nextFloat();
+//         System.out.println("Hello " + fName + ", your grade is " + grade + ".");
+//     }
+// }
+
+// Task - 4
+// Abdul Basit Ali : 25SP-009-ST
+// import java.util.Scanner;
+// public class Main{
+//     public static void main(String[] args){
+//         Scanner in = new Scanner(System.in);
+//         System.out.print("Enter role number : ");
+//         int roleNumber = in.nextInt();
+//         System.out.print("Enter grade : ");
+//         Double grade = in.nextDouble();
+//         in.nextLine();
+//         System.out.print("Enter name : ");
+//         String name = in.nextLine();
+//         System.out.println("Role-Number : " + roleNumber + "\nGrade : " + grade + "\nName : " + name);
+//     }
+// }
+
