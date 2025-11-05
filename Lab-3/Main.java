931727c955cbe7be6135ab461e62d6e515c4f5a5
Abdul Basit Ali <240479325+UITU-25SP-009-ST@users.noreply.github.com>
2025-11-05 20:18:05 +0000

    Lab-3 Completed

--- Lab-3/Main.java
+++ Lab-3/Main.java
@@ -48,3 +48,26 @@
 
 // Task - 4
 // Abdul Basit Ali : 25SP-009-ST
+// import java.util.Scanner;
+// public class Main{
+//     public static void main(String[] args) {
+//         Scanner s = new Scanner(System.in);
+//         System.out.print("Enter a number : ");
+//         int m = s.nextInt();
+//         switch (m) {
+//             case 1: System.out.println("January"); break;
+//             case 2: System.out.println("February"); break;
+//             case 3: System.out.println("March"); break;
+//             case 4: System.out.println("April"); break;
+//             case 5: System.out.println("May"); break;
+//             case 6: System.out.println("June"); break;
+//             case 7: System.out.println("July"); break;
+//             case 8: System.out.println("August"); break;
+//             case 9: System.out.println("September"); break;
+//             case 10: System.out.println("October"); break;
+//             case 11: System.out.println("November"); break;
+//             case 12: System.out.println("December"); break;
+//             default: System.out.println("Invalid Month");
+//         }
+//     }
+// }
