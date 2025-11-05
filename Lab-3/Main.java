3353b005a2b9d6682cee9a3eda43b18f481a775d
Abdul Basit Ali <240479325+UITU-25SP-009-ST@users.noreply.github.com>
2025-11-05 19:48:55 +0000

    Task-3 Completed

--- Lab-3/Main.java
+++ Lab-3/Main.java
@@ -15,14 +15,36 @@
 
 // Task - 2
 // Abdul Basit Ali : 25SP-009-ST
-import java.util.Scanner;
-    public class Main{
-        public static void main(String[] args){
-            Scanner in = new Scanner(System.in);
-            System.out.print("Enter first number : ");
-            int a = in.nextInt();
-            System.out.print("Enter first number : ");
-            int b = in.nextInt();
-            System.out.println(a > b ? a + " is Greater" : a < b ? b + " is Greater" : "Equal");
-        }
-    }
+// import java.util.Scanner;
+//     public class Main{
+//         public static void main(String[] args){
+//             Scanner in = new Scanner(System.in);
+//             System.out.print("Enter first number : ");
+//             int a = in.nextInt();
+//             System.out.print("Enter first number : ");
+//             int b = in.nextInt();
+//             System.out.println(a > b ? a + " is Greater" : a < b ? b + " is Greater" : "Equal");
+//         }
+//     }
+
+
+// Task - 3
+// Abdul Basit Ali : 25SP-009-ST
+// import java.util.Scanner;
+// public class Main{
+//     public static void main(String[] args){
+//         Scanner in = new Scanner(System.in);
+//         System.out.print("Enter Percentage : ");
+//         int grade = in.nextInt();
+//        String gradeLetter = (grade >= 90 && grade <= 100) ? "A" :
+//                      (grade >= 80 && grade < 90) ? "B" :
+//                      (grade >= 70 && grade < 80) ? "C" :
+//                      (grade >= 60 && grade < 70) ? "D" : 
+//                      "F";
+//         System.out.println("Your Grade is " + gradeLetter);
+//     }
+// }
+
+
+// Task - 4
+// Abdul Basit Ali : 25SP-009-ST
