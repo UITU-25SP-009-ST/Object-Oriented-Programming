54ea276bb64f74f0b2aa21ffb2c025035cbc5226
Abdul Basit Ali <240479325+UITU-25SP-009-ST@users.noreply.github.com>
2025-11-06 10:43:41 +0000

    Updated

--- Lab-3/Main.java
+++ Lab-3/Main.java
@@ -21,7 +21,7 @@
 //             Scanner in = new Scanner(System.in);
 //             System.out.print("Enter first number : ");
 //             int a = in.nextInt();
-//             System.out.print("Enter first number : ");
+//             System.out.print("Enter second number : ");
 //             int b = in.nextInt();
 //             System.out.println(a > b ? a + " is Greater" : a < b ? b + " is Greater" : "Equal");
 //         }
@@ -48,26 +48,26 @@
 
 // Task - 4
 // Abdul Basit Ali : 25SP-009-ST
-// import java.util.Scanner;
-// public class Main{
-//     public static void main(String[] args) {
-//         Scanner s = new Scanner(System.in);
-//         System.out.print("Enter a number : ");
-//         int m = s.nextInt();
-//         switch (m) {
-//             case 1: System.out.println("January"); break;
-//             case 2: System.out.println("February"); break;
-//             case 3: System.out.println("March"); break;
-//             case 4: System.out.println("April"); break;
-//             case 5: System.out.println("May"); break;
-//             case 6: System.out.println("June"); break;
-//             case 7: System.out.println("July"); break;
-//             case 8: System.out.println("August"); break;
-//             case 9: System.out.println("September"); break;
-//             case 10: System.out.println("October"); break;
-//             case 11: System.out.println("November"); break;
-//             case 12: System.out.println("December"); break;
-//             default: System.out.println("Invalid Month");
-//         }
-//     }
-// }
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int m = s.nextInt();
+        switch (m) {
+            case 1: System.out.println("January"); break;
+            case 2: System.out.println("February"); break;
+            case 3: System.out.println("March"); break;
+            case 4: System.out.println("April"); break;
+            case 5: System.out.println("May"); break;
+            case 6: System.out.println("June"); break;
+            case 7: System.out.println("July"); break;
+            case 8: System.out.println("August"); break;
+            case 9: System.out.println("September"); break;
+            case 10: System.out.println("October"); break;
+            case 11: System.out.println("November"); break;
+            case 12: System.out.println("December"); break;
+            default: System.out.println("Invalid Month");
+        }
+    }
+}
